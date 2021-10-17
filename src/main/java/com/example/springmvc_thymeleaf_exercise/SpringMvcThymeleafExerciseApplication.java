704b1eda447cab432004b22c05c5599da517a98d
Abbe-Mohamud <mohamud99@gmail.com>
2021-10-17 08:08:50 +0200

    Initial commit

---
+++ src/main/java/com/example/springmvc_thymeleaf_exercise/SpringMvcThymeleafExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmvc_thymeleaf_exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcThymeleafExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcThymeleafExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springmvc_thymeleaf_exercise/SpringMvcThymeleafExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmvc_thymeleaf_exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcThymeleafExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
